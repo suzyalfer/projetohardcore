926ca495472fbe49320cb8b60e33d8c2788747c9
suzyalfer <105470223+suzyalfer@users.noreply.github.com>
2024-03-02 15:35:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+//alert('JS CARREGADO');
+
+//Seleção de DOM
+
+var btnMenu = document.querySelector('.btn-menu');
+var menu = document.querySelector('.menu');
+var btnPop = document.querySelector('.btn-pop');
+var pop = document.querySelector('.pop');
+
+
+
+//console.log(btnMenu);
+//console.log(menu);
+
+//Eventos (são ações geralmente causadas pelo usuário)
+
+
+/*
+function boasVindas(){
+
+    alert('Olá');
+}
+
+btnMenu.addEventListener('click', boasVindas);
+
+btnMenu.addEventListener('click', function(){
+    alert('olá2');
+
+});
+*/
+
+btnMenu.addEventListener('click', function(){
+
+    //add ou remove a classe menu-open
+    menu.classList.toggle('menu-open');
+    btnMenu.classList.toggle('x');
+    
+    
+})
+
+menu.addEventListener('click', function(){
+
+    menu.classList.remove('menu-open');
+    btnMenu.classList.remove('x');
+       
+    
+})
+
+
+btnPop.addEventListener('click', function(){
+
+    pop.classList.replace('pop','popClose');
+       
+    
+})
+
